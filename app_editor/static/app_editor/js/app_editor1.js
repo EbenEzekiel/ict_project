4cbb96d2de32cad36773cd321de415906698659d
Ebenezer <ebenezerconfide@gmail.com>
2026-05-16 18:17:24 +0100

    Added remove button beside 'file input' option

--- app_editor/static/app_editor/js/app_editor1.js
+++ app_editor/static/app_editor/js/app_editor1.js
@@ -1,22 +1,28 @@
+// declare variables
 let fileInput = document.getElementById("file-input");
 let receivedDuration = document.getElementById("received-duration");
 let times = document.getElementById("times");
+let workingFile = document.getElementById("working-file");
 
 let get_duration = () => {
 
-    fetch("http://127.0.0.1:8000/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        // your payload here
-        "file": fileInput.value
-      })
+  // set the working file text content to the value of the file input
+  workingFile.textContent = fileInput.value;
+
+  fetch("http://127.0.0.1:8000/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      // payload here
+      "file": fileInput.value
     })
-    .then(response => response.text())
-    .then(content => times.innerHTML = content)
-    .catch(error => times.innerHTML = "<h3>An error occured, contact Site Admin</h3>");
+  })
+  .then(response => response.text())
+  .then(content => times.innerHTML = content)
+  .catch(error => times.innerHTML = "<h3><i>An error occured, contact Site Admin...</i></h3>");
+
 }
 
 fileInput.addEventListener("change", get_duration);
