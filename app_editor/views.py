a69e3ec8ff3ca1a39a4f3edb28fd67b324d5d783
Ebenezer <ebenezerconfide@gmail.com>
2026-05-25 04:30:55 +0100

    'process' view now minimally functional

--- app_editor/views.py
+++ app_editor/views.py
@@ -44,13 +44,77 @@ def home(request):
 
 @csrf_exempt
 def process(request):
+    # get data from frontend
     data = json.loads(request.body)
+    wf =  data.get("working-file")
+    duration = int(float(data.get("duration")))
+    
+    wf =  media_root + f"/{wf}"
+    video = mpy.VideoFileClip(wf)
+    
+    # load video file
+    video = mpy.VideoFileClip(wf)
+    # get time range dictionary
+    if duration < 60:
+        time_dict = {
+            "testimony": [f"{data.get("t1-sec-testimony")}", f"{data.get("t2-sec-testimony")}"],
+            "choir": [f"{data.get("t1-sec-choir")}", f"{data.get("t2-sec-choir")}"],
+            "sermon": [f"{data.get("t1-sec-sermon")}", f"{data.get("t2-sec-sermon")}"], 
+        }
+    elif duration < 3600:
+        time_dict = {
+            "testimony": [f"{data.get("t1-min-testimony")}:{data.get("t1-sec-testimony")}", 
+                          f"{data.get("t2-min-testimony")}:{data.get("t2-sec-testimony")}"],
 
-    #get titles
-    # title_testimony = data.get('title-testimony')
-    # title_choir = data.get('title-choir')
-    # title_sermon = data.get('title-sermon')
-    print("Received from frontend:")
-    for k,v in data.items():
-        print(k, " =>  ", v)
+            "choir": [f"{data.get("t1-min-choir")}:{data.get("t1-sec-choir")}", 
+                      f"{data.get("t2-min-choir")}:{data.get("t2-sec-choir")}"],
+
+            "sermon": [f"{data.get("t1-min-sermon")}:{data.get("t1-sec-sermon")}",
+                        f"{data.get("t2-min-sermon")}:{data.get("t2-sec-sermon")}"], 
+        }
+    else:
+        time_dict = {
+            "testimony": [f"{data.get("t1-hr-testimony")}:{data.get("t1-min-testimony")}:{data.get("t1-sec-testimony")}", 
+                          f"{data.get("t2-hr-testimony")}:{data.get("t2-min-testimony")}:{data.get("t2-sec-testimony")}"],
+
+            "choir": [f"{data.get("t1-hr-testimony")}:{data.get("t1-min-choir")}:{data.get("t1-sec-choir")}", 
+                      f"{data.get("t2-hr-testimony")}:{data.get("t2-min-choir")}:{data.get("t2-sec-choir")}"],
+
+            "sermon": [f"{data.get("t1-hr-testimony")}:{data.get("t1-min-sermon")}:{data.get("t1-sec-sermon")}",
+                       f"{data.get("t1-hr-testimony")}:{data.get("t2-min-sermon")}:{data.get("t2-sec-sermon")}"], 
+        }
+
+    # #process audio files
+    if data.get("options-testimony") == 'audio' or data.get("options-testimony") == 'audiovideo':
+        video.subclipped(time_dict["testimony"][0] , 
+                         time_dict["testimony"][1]
+                         ).audio.write_audiofile(f"{media_root}/output/{data.get("title-testimony")}.mp3")
+
+    if data.get("options-choir") == 'audio' or data.get("options-choir") == 'audiovideo':
+        video.subclipped(time_dict["choir"][0] , 
+                         time_dict["choir"][1]
+                         ).audio.write_audiofile(f"{media_root}/output/{data.get("title-choir")}.mp3")
+
+    if data.get("options-sermon") == 'audio' or data.get("options-sermon") == 'audiovideo':
+        video.subclipped(time_dict["sermon"][0] , 
+                         time_dict["sermon"][1]
+                         ).audio.write_audiofile(f"{media_root}/output/{data.get("title-sermon")}.mp3")
+
+
+    #process video files
+    if data.get("options-testimony") == 'video' or data.get("options-testimony") == 'audiovideo':
+        video.subclipped(time_dict["testimony"][0] , 
+                         time_dict["testimony"][1]
+                         ).write_videofile(f"{media_root}/output/{data.get("title-testimony")}.mp4")
+
+    if data.get("options-choir") == 'video' or data.get("options-choir") == 'audiovideo':
+        video.subclipped(time_dict["choir"][0] , 
+                         time_dict["choir"][1]
+                         ).write_videofile(f"{media_root}/output/{data.get("title-choir")}.mp4")
+
+    if data.get("options-sermon") == 'video' or data.get("options-sermon") == 'audiovideo':
+        video.subclipped(time_dict["sermon"][0] , 
+                         time_dict["sermon"][1]
+                         ).write_videofile(f"{media_root}/output/{data.get("title-sermon")}.mp4")
+
     return JsonResponse({"message": "Success"})
